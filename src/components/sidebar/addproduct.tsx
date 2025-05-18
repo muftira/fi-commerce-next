964cabcb1e10ddb49f2d5f3fa2ef1ae50e52197f
muftira <muh.muftir@gmail.com>
2025-05-18 15:07:58 +0700

    updated functionality photo component

--- src/components/sidebar/addproduct.tsx
+++ src/components/sidebar/addproduct.tsx
@@ -6,24 +6,38 @@ import { Label } from '@/components/ui/label';
 import Asterisk from '@/components/asterisk';
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
+import Select from 'react-select';
+import { MdOutlineAddPhotoAlternate } from 'react-icons/md';
+
+// Data
+import { options } from '@/utils/data';
 
 export default function addproduct() {
   const [images, setImages] = useState<File[]>([]);
   const [previews, setPreviews] = useState<string[]>([]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangePhoto = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
     if (!files) return;
 
-    const selectedFiles = Array.from(files).slice(0, 3); // batasi hanya 3
+    if (images.length + files.length > 3) {
+      alert('You can only upload up to 3 images.');
+      return;
+    }
+
+    const selectedFiles = Array.from(files).slice(0, 3);
     setImages(selectedFiles);
-
-    const previewUrls = selectedFiles.map((file) => URL.createObjectURL(file));
+    let DataImages = [...images];
+    if (selectedFiles.length > 0) {
+      selectedFiles.forEach((file) => {
+        DataImages.push(file);
+      });
+    }
+    setImages(DataImages);
+    const previewUrls = DataImages.map((file) => URL.createObjectURL(file));
     setPreviews(previewUrls);
-    console.log('preview photo==>', files);
-
   };
-  
+  console.log('images ==>', images);
   return (
     <div className="p-4">
       <p className="text-4xl font-bold mb-10">Add Your Product</p>
@@ -38,24 +52,36 @@ export default function addproduct() {
           <Label className="font-bold" htmlFor="Category">
             Category <Asterisk />
           </Label>
-          <Input className="w-[900px]" id="Category" placeholder="Category" />
+          <Select className="w-[900px]" options={options} />
         </div>
-        <div className="flex flex-row">
-        <Label className="font-bold" htmlFor="product's name">
-            Photo <Asterisk />
+        <div className="flex items-start mt-[-15px] flex-row">
+          <Label className="font-bold mt-5" htmlFor="photoProduct">
+            Photo Product <Asterisk />
           </Label>
-          <div className='ml-[165px]'>
-          <input type="file" accept="image/png, image/jpeg" multiple onChange={handleChange} />
-          <div className="flex gap-2 mt-4">
-            {previews.map((src, index) => (
-              <img
-                key={index}
-                src={src}
-                alt={`Preview ${index + 1}`}
-                className="w-24 h-24 object-cover border"
-              />
-            ))}
-          </div>
+          <div className="flex ml-[105px]">
+            <div className="flex gap-2 mt-4">
+              {previews.map((src, index) => (
+                <img
+                  className="w-24 h-24 object-cover border rounded-md"
+                  key={index}
+                  src={src}
+                  alt={`Preview ${index + 1}`}
+                />
+              ))}
+              <div className="relative">
+                <div className="flex flex-col justify-center items-center gap-3 w-24 h-24 cursor-pointer border border-dashed border-gray-400 rounded-md absolute top-0">
+                  <MdOutlineAddPhotoAlternate className="text-4xl" />
+                  <p className="text-[10px]">{`Add Photo (${images.length}/3)`}</p>
+                </div>
+                <input
+                  className="w-24 h-24 cursor-pointer opacity-0"
+                  type="file"
+                  accept="image/png, image/jpeg"
+                  multiple
+                  onChange={handleChangePhoto}
+                />
+              </div>
+            </div>
           </div>
         </div>
         <div className="flex flex-row justify-between">
